0dde8e5726b3b613f37910c6f13f152ad3037761
Cai Jiang <luffy.ja@gmail.com>
2018-02-28 02:46:03 +0800

    遇到431那么reload呗

--- src/utils/error.js
+++ src/utils/error.js
@@ -3,6 +3,9 @@ import { routerRedux } from 'dva/router';
 const error = (e, dispatch) => {
   // 还在登录页面就不搞事了
   if (location.href.indexOf('/login/login') > 0) { return; }
+  if (e.response && e.response.status === 431) {
+    window.location.reload();
+  }
   // if (e.response && e.response.url.indexOf('manageLoginResult') >= 0) { return; }
   // if (e.response && e.response.url.indexOf('managerLoginRequest') >= 0) { return; }
   dispatch(routerRedux.push('/exception/500'));
